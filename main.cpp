936b4a7265b53c1a6ef5ad1d36f2af1d3d06667d
redondelos <carlos.puente.r@outlook.com>
2026-08-13 17:14:06 -0500

    Modified network

--- main.cpp
+++ main.cpp
@@ -43,10 +43,10 @@ void printData(float temperature, float humidity, float atmPressure);
 // Create a web server on port 80
 WebServer server(80);
 
-const char *ssid = "chuck";
-const char *password = "cpuente2026";
-// const char *ssid = "NETLIFE-PUENTE";
-// const char *password = "LosElenes1996";
+// const char *ssid = "chuck";
+// const char *password = "cpuente2026";
+const char *ssid = "NETLIFE-PUENTE";
+const char *password = "LosElenes1996";
 void handleRoot();
 void handleControlPanel();
 void handleCSS();
